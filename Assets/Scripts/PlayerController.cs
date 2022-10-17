5fdeacee8204f0fcb27628608637f4cac680b0bc
ArtisanGray <kitwithshorts@gmail.com>
2022-10-17 15:15:47 -0500

    created tasks 0-11, check commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     private int score = 0;
     public int health;
+    public Text scoreText;
+    public Text healthText;
+    public Text goalText;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +22,16 @@ public class PlayerController : MonoBehaviour
     {
         if (health == 0)
         {
-            Debug.Log("Game over!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //Debug.Log("Game over!");
+            goalText.transform.parent.gameObject.SetActive(true);
+            goalText.text = "Game Over!";
+            goalText.color = Color.white;
+            goalText.GetComponentInParent<Image>().color = Color.red;
+            StartCoroutine(LoadScene(3));
+        }
+        if (Input.GetKeyDown("escape"))
+        {
+            SceneManager.LoadScene("menu");
         }
     }
     // Update is called once per frame
@@ -48,17 +60,37 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.tag == "Pickup")
         {
             score += 1;
-            Debug.Log("Score: " + score);
+            //Debug.Log("Score: " + score);
+            SetScoreText();
             Destroy(other.gameObject);
         }
         if (other.gameObject.tag == "Trap")
         {
             health -= 1;
-            Debug.Log("Health: " + health);
+            SetHealthText();
+            //Debug.Log("Health: " + health);
         }
         if (other.gameObject.tag == "Goal")
         {
-            Debug.Log("You win!");
+            //Debug.Log("You win!");
+            goalText.transform.parent.gameObject.SetActive(true);
+            goalText.text = "You Win!";
+            goalText.GetComponentInParent<Image>().color = Color.green;
+            goalText.color = Color.black;
+
         }
+    }
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health;
+    }
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
